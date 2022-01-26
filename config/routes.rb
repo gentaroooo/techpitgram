c1b9e7876cb5565a2fca09d3895d561a29ba570a
gentaro.yamazaki <going3956@yahoo.co.jp>
2022-01-27 00:01:13 +0900

    de5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
+  # ここにusersコントローラーのshowアクションのルーティングを追加する
+  get '/users/:id', to: 'users#show', as: 'user'
 end
