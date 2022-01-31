0d2ff070198b5a7c30160cd10a402dc1692e644b
gentaro.yamazaki <going3956@yahoo.co.jp>
2022-01-31 23:44:10 +0900

    post2

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,5 @@
 class Photo < ApplicationRecord
+ # ここに追加する
+ belongs_to :post
+ validates :image, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
      # ここに追加する
   belongs_to :user
+  has_many :photos, dependent: :destroy
 end
