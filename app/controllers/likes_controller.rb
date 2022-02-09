72c8a5b7b299b719ef5bb093bb64bd4c6e3d6ded
gentaro.yamazaki <going3956@yahoo.co.jp>
2022-02-09 20:34:42 +0900

    like_add2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,25 @@
 class LikesController < ApplicationController
-end
+    def create
+      @like = current_user.likes.build(like_params)
+      @post = @like.post
+      if @like.save
+        respond_to :js
+      end
+    end
+  
+    # ==========ここから追加する==========
+    def destroy
+      @like = Like.find_by(id: params[:id])
+      @post = @like.post
+      if @like.destroy
+        respond_to :js
+      end
+    end
+    # ==========ここまで追加する==========
+  
+    private
+      def like_params
+        params.permit(:post_id)
+      end
+  end
+  
