1294fff0c0beae8aa7a571c3673cab607bd057fa
gentaro.yamazaki <going3956@yahoo.co.jp>
2022-02-11 19:29:30 +0900

    comment3

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,9 @@ class Post < ApplicationRecord
 
   accepts_nested_attributes_for :photos
 
-  # ==========ここから追加する==========
   def liked_by(user)
     # user_idとpost_idが一致するlikeを検索する
     Like.find_by(user_id: user.id, post_id: id)
   end
-  # ==========ここまで追加する==========
 
 end
